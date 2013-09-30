635c179fc4a0dddebf4f180d8b3a264572c24934
paul.thornton@easy-ip.net <paul.thornton@easy-ip.net@35a5af27-5b28-9ebe-25c5-01ba9371e2e8>
2013-09-30 15:29:14 +0000

    Added XE4 Packages

---
+++ trunk/Demo/ChromeTabsDemoXE4.dpr
@@ -0,0 +1,25 @@
+program ChromeTabsDemoXE4;
+
+uses
+  EMemLeaks,
+  EResLeaks,
+  EDialogWinAPIMSClassic,
+  EDialogWinAPIEurekaLogDetailed,
+  EDialogWinAPIStepsToReproduce,
+  EDebugExports,
+  EDebugJCL,
+  EAppVCL,
+  ExceptionLog7,
+  Forms,
+  frmMainU in 'frmMainU.pas' {frmMain},
+  ChromeTabsLog in '..\Lib\ChromeTabsLog.pas',
+  frameChromeTabStyleU in 'frameChromeTabStyleU.pas' {frameChromeTabStyle: TFrame};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
